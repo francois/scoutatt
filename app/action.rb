eb1a21b697cfbc81cf8977c1e88648808fae0197
François Beausoleil <francois@teksol.info>
2024-08-11 14:30:12 -0400

    Properly log errors to recover from them

--- app/action.rb
+++ app/action.rb
@@ -8,5 +8,25 @@ module Scoutatt
   class Action < Hanami::Action
     # Provide `Success` and `Failure` for pattern matching on operation results
     include Dry::Monads[:result]
+    include Deps['logger']
+
+    handle_exception StandardError => :handle_standard_error
+
+    def handle_standard_error(request, response, exception)
+      if Hanami.env?(:development)
+        raise exception
+      else
+        logger.error do
+          {
+            class_name: exception.class.name,
+            message: exception.message,
+            backtrace: exception.backtrace
+          }.to_json
+        end
+
+        response.status = 500
+        response.body = "Sorry, something went wrong handling your request"
+      end
+    end
   end
 end
